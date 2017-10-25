5c4dfb33dcafccc718c23c63346258b38a13bec9
rlang <rlang@172.18.36.245>
2017-10-25 15:05:50 +0200

    implémentation linkedlist

---
+++ Archi_Log/src/tp1/FileLinkedList.java
@@ -0,0 +1,29 @@
+package tp1;
+
+import java.util.LinkedList;
+
+public class FileLinkedList<T> implements File<T> {
+	
+	private LinkedList<T> liste ;
+	
+	public FileLinkedList(LinkedList<T> t){
+		this.liste = t ;
+	}	
+	public LinkedList<T> getListe() {
+		return liste;
+	}
+
+	public boolean estvide() {		
+		return this.getListe().isEmpty();
+	}
+
+	public void push(T e) {
+		this.getListe().add(e);
+	}
+
+	
+	public T pull() {
+		return this.getListe().pop();
+	}
+
+}
