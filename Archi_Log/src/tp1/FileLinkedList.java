33d455d9b31cc99aa85c233b45ef56002cef5380
rlang <rlang@172.18.36.245>
2017-10-25 15:39:21 +0200

    avec des services

--- Archi_Log/src/tp1/FileLinkedList.java
+++ Archi_Log/src/tp1/FileLinkedList.java
@@ -2,13 +2,14 @@ package tp1;
 
 import java.util.LinkedList;
 
-public class FileLinkedList<T> implements File<T> {
+public class FileLinkedList<T> extends ServiceFile<T> {
 	
 	private LinkedList<T> liste ;
 	
 	public FileLinkedList(LinkedList<T> t){
 		this.liste = t ;
-	}	
+	}
+	
 	public LinkedList<T> getListe() {
 		return liste;
 	}
@@ -21,7 +22,6 @@ public class FileLinkedList<T> implements File<T> {
 		this.getListe().add(e);
 	}
 
-	
 	public T pull() {
 		return this.getListe().pop();
 	}
---
+++ Archi_Log/src/tp1/ServiceFile.java
@@ -0,0 +1,31 @@
+package tp1;
+
+public abstract class ServiceFile<T> implements File<T> {
+	
+	public String toString(){
+		if(this.estvide()){
+			return "" ;
+		}
+		else {
+			return "" + this.pull().toString() ;
+		}
+	}
+	
+	public int size(){
+		if(this.estvide()){
+			return 0 ;
+		}
+		else {
+			this.pull();
+			return 1 + (this.size());
+		}
+	}
+	
+	public boolean contient(T e){
+		if(this.estvide()){
+			return false ;
+		}
+		else return (this.pull().equals(e))|(this.contient(e));
+	}
+
+}
