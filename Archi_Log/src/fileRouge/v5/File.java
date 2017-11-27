4a0a0817015be3145b81e604926e51b243039dec
michal-bartlett <michal.g.bartlett@gmail.com>
2017-11-27 17:54:32 +0100

    voila

--- Archi_Log/src/fileRouge/v5/File.java
+++ Archi_Log/src/fileRouge/v5/File.java
@@ -1,12 +1,26 @@
 package fileRouge.v5;
 
-public interface File<K, E> extends Iterable, Mesurable {
+public interface File<K extends File<K, E>, E> extends Iterable<E>, Mesurable {
 	
 	
 	E premier(); // Premier de la file
 	K suivants(); // Ses suivants
 	//boolean estVide();
 	default boolean estVide(){
-		return false;
+		return true;
+	}
+	
+	K creer(); // fabrique d'une file vide
+	@SuppressWarnings("unchecked")
+	default K sujet(){ // Alias de this
+	  return (K)this;
+	}
+	
+	K ajout(E dernierDansFile); // Ajout en fin
+	K retrait(); // Retrait de premier élément	
+	default K ajoute(K secondFile){
+		//this.
+		return secondFile;
 	}
 }
+
